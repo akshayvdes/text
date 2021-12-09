1633dd9b260bc51df325d451ff309c7a508bb6ef
akshayvdes <vd.akshaykumar@gmail.com>
2021-12-09 10:08:10 +0000

    changes don on rename script

--- rename.sh
+++ rename.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-ls *.html > output
+ls *.txt > output
 while read line
 do
 	var=` echo "$line" | awk -F "." '{print $1}'`
-	mv $line $var.txt
+	mv $line $var.html
 	 
 done < output
 
